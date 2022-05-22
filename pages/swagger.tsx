aa7b06f5285e717fafd6a2a021e8750e892828c5
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-05-22 15:15:43 +0200

    Fix background color

--- pages/swagger.tsx
+++ pages/swagger.tsx
@@ -1,14 +1,21 @@
 import { GlobalHead } from "../components/GlobalHead";
 import "swagger-ui-react/swagger-ui.css";
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
 
 const SwaggerUI = dynamic(import("swagger-ui-react"), { ssr: false });
 
 export default function Swagger() {
+  useEffect(() => {
+    document.getElementsByTagName("html")[0].className = "bg-white";
+  }, []);
+
   return (
     <>
       <GlobalHead />
-      <SwaggerUI url="/api/backend/openapi.json" />
+      <div className="bg-white">
+        <SwaggerUI url="/api/backend/openapi.json" />
+      </div>
     </>
   );
 }
