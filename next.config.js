3dff67560930ae4d5617fdc5a700150771a87d93
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-08-23 17:05:45 +0200

    Fix build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development"
+});
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 });
@@ -6,12 +11,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development"
-  },
   i18n: {
     locales: ["en-US"],
     defaultLocale: "en-US",
