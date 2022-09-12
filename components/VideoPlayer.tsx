ce341e55752ad5d548849d83fecc2bb4035fe634
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-09-12 16:25:46 +0200

    Fix build v2

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,5 +1,5 @@
 import Plyr from "plyr-react";
-import "plyr-react/dist/plyr.css";
+import "plyr-react/plyr.css";
 import { VideoResponse } from "../lib/responses";
 
 export default function VideoPlayer({
