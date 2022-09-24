44fa7d79c4d7c873f03c2944c97fca6ef1b98f53
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-09-24 11:23:33 +0200

    Implement infinite loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,46 @@
-import type { NextPage } from "next";
-import { GlobalHead } from "../components/GlobalHead";
+import type {NextPage} from "next";
+import {GlobalHead} from "../components/GlobalHead";
 import Layout from "../components/Layout";
 import LoadingView from "../components/LoadingView";
 import PostCard from "../components/PostCard";
-import { PostResponse } from "../lib/responses";
+import {PostResponse} from "../lib/responses";
 import Masonry from "react-masonry-css";
-import { breakpointColumnsObj } from "../lib/shared";
-import useSWR from 'swr'
+import {breakpointColumnsObj} from "../lib/shared";
+import useSWRInfinite, {SWRInfiniteKeyLoader} from 'swr/infinite'
+
+const getKey: SWRInfiniteKeyLoader = (pageIndex, previousPageData) => {
+  if (previousPageData && !previousPageData.length) return null
+  return `/home?page=${pageIndex}`
+}
 
 const Home: NextPage = () => {
-  const { data, error } = useSWR<PostResponse[]>('/home', { refreshInterval: 60000 })
+  const {data, size, setSize} = useSWRInfinite<PostResponse[]>(getKey)
 
   if (data) {
     return (
-      <>
-        <GlobalHead />
-        <Layout>
-          <div className="container flex-grow p-1 md:p-6">
-            <h1 className="text-2xl font-bold mx-2">Recent posts...</h1>
-            <Masonry
-              breakpointCols={breakpointColumnsObj}
-              className="my-masonry-grid"
-              columnClassName="my-masonry-grid_column"
-            >
-              {data.map((post, index) => (
-                <PostCard key={index} post={post} />
-              ))}
-            </Masonry>
-          </div>
-        </Layout>
-      </>
+        <>
+          <GlobalHead/>
+          <Layout>
+            <div className="container flex-grow p-1 md:p-6">
+              <h1 className="text-2xl font-bold mx-2">Recent posts...</h1>
+              <Masonry
+                  breakpointCols={breakpointColumnsObj}
+                  className="my-masonry-grid"
+                  columnClassName="my-masonry-grid_column"
+              >
+                {data.map((posts, postsIndex) => (
+                    posts.map((post, postIndex) => (
+                        <PostCard key={`${postsIndex}-${postIndex}`} post={post}/>
+                    ))
+                ))}
+              </Masonry>
+              <button onClick={() => setSize(size + 1)}>Load More</button>
+            </div>
+          </Layout>
+        </>
     );
   } else {
-    return <LoadingView />;
+    return <LoadingView/>;
   }
 };
 
