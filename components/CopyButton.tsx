49422f0a2d31e2e5f7fbe041560d9df773ff8e5f
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-09-25 21:05:39 +0200

    Add copy button

---
+++ components/CopyButton.tsx
@@ -0,0 +1,21 @@
+import {cloneElement, ReactElement, useState} from "react";
+import copy from "copy-to-clipboard";
+
+export default function CopyButton({
+                                     text,
+                                     children
+                                   }: { text: string, children: ReactElement<any, any> }) {
+  const [copied, setCopied] = useState(false);
+
+  const handleClick = () => {
+    copy(text);
+    setCopied(true)
+  }
+  const handleMouseLeave = () => setTimeout(() => setCopied(false), 300)
+
+  return (
+      <span className="tooltip z-9999" data-tip={copied ? "Copied!" : "Click to copy!"}>
+        {cloneElement(children, {onClick: handleClick, onMouseLeave: handleMouseLeave})}
+      </span>
+  )
+}
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -2,7 +2,7 @@ import {GetServerSideProps} from "next";
 import {GlobalHead} from "../../components/GlobalHead";
 import Layout from "../../components/Layout";
 import {useRouter} from "next/router";
-import {useContext, useState} from "react";
+import {ReactNode, useContext, useState} from "react";
 import {PostResponse, UserData} from "../../lib/responses";
 import Image from "next/image";
 import ReactTimeAgo from "react-time-ago";
@@ -15,8 +15,17 @@ import PostCommentForm from "../../components/PostCommentForm";
 import Head from "next/head";
 import axios from "../../lib/axios";
 import {VoteType} from "../../lib/types";
+import CopyButton from "../../components/CopyButton";
 
 const VideoPlayer = dynamic(() => import("../../components/VideoPlayer"));
+
+const PostOptionColumn = ({children}: { children: ReactNode }) => {
+  return (
+      <div className="flex flex-col justify-center">
+        {children}
+      </div>
+  )
+}
 
 const VoteRow = ({postData, user}: { postData: PostResponse, user: UserData | undefined }) => {
   const [likes, setLikes] = useState(postData.likes);
@@ -65,7 +74,7 @@ const VoteRow = ({postData, user}: { postData: PostResponse, user: UserData | un
   )
 }
 
-const Post = ({postData}: { postData: PostResponse }) => {
+const Post = ({postData, postUrl}: { postData: PostResponse, postUrl: string }) => {
   const {user} = useContext(UserDataContext);
   const router = useRouter();
   const {image} = router.query;
@@ -75,12 +84,16 @@ const Post = ({postData}: { postData: PostResponse }) => {
           ? Math.min(postData.images!.length - 1, parseInt(image as string))
           : 0;
 
-  const imageUrl =
-      postData.type === "IMAGES"
-          ? `/backend/static/images/${postData.images![imageIndex].id}.${
-              postData.images![imageIndex].extension
-          }`
-          : undefined;
+  const imageData: {
+    url: string;
+    width: number;
+    height: number;
+  } | undefined = postData.type === "IMAGES" ? {
+    url: `/backend/static/images/${postData.images![imageIndex].id}.${postData.images![imageIndex].extension}`,
+    width: postData.images![imageIndex].width,
+    height: postData.images![imageIndex].height,
+  } : undefined;
+
   const videoUrl =
       postData.type === "VIDEO"
           ? `/backend/static/videos/${postData.video!.id}.${
@@ -123,19 +136,19 @@ const Post = ({postData}: { postData: PostResponse }) => {
             noImage={postData.type === "IMAGES" || postData.type === "VIDEO"}
         />
         <Layout>
-          {postData.type === "IMAGES" && (
+          {postData.type === "IMAGES" && imageData && (
               <Head>
                 <meta name="twitter:card" content="summary_large_image"/>
 
-                <meta property="og:image" content={imageUrl}/>
-                <meta name="twitter:image" content={imageUrl}/>
+                <meta property="og:image" content={imageData.url}/>
+                <meta name="twitter:image" content={imageData.url}/>
                 <meta
                     property="og:image:width"
-                    content={postData.images![imageIndex].width.toString()}
+                    content={imageData.width.toString()}
                 />
                 <meta
                     property="og:image:height"
-                    content={postData.images![imageIndex].height.toString()}
+                    content={imageData.height.toString()}
                 />
               </Head>
           )}
@@ -170,55 +183,72 @@ const Post = ({postData}: { postData: PostResponse }) => {
           <div className="break-text container flex-grow p-2">
             <div className="rounded-box flex w-full flex-wrap bg-base-200 p-4 p-4">
               <h2 className="my-2 text-2xl font-bold">{postData.title}</h2>
-              <div className="flex flex-wrap gap-1">
+              <div className="flex flex-wrap gap-1 ml-1">
                 {postData.tags.map((tag) => (
-                    <div key={tag} className="flex flex-col justify-center">
+                    <PostOptionColumn key={tag}>
                       <Link href={"/tag/" + tag}>
-                        <a className="badge badge-lg ml-1.5 p-2">#{tag}</a>
+                        <a className="badge badge-lg">#{tag}</a>
                       </Link>
-                    </div>
+                    </PostOptionColumn>
                 ))}
                 {postData.unlisted && (
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="my-auto ml-1.5 h-6 w-6"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                      <title>Unlisted</title>
-                      <path
-                          fillRule="evenodd"
-                          d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
-                          clipRule="evenodd"
-                      />
-                      <path
-                          d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"/>
-                    </svg>
+                    <PostOptionColumn>
+                      <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                      >
+                        <title>Unlisted</title>
+                        <path
+                            fillRule="evenodd"
+                            d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
+                            clipRule="evenodd"
+                        />
+                        <path
+                            d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"/>
+                      </svg>
+                    </PostOptionColumn>
                 )}
                 {user && (
                     <>
                       {(user.roles.includes("ADMIN") ||
                           postData.authorData.id === user.id) && (
-                          <Link href={`/post/${postData.postId}/edit`}>
-                            <a className="my-auto ml-1.5">
-                              <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  className="h-6 w-6"
-                                  viewBox="0 0 20 20"
-                                  fill="currentColor"
-                              >
-                                <title>Edit</title>
-                                <path
-                                    fillRule="evenodd"
-                                    d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                                    clipRule="evenodd"
-                                />
-                              </svg>
-                            </a>
-                          </Link>
+                          <PostOptionColumn>
+                            <Link href={`/post/${postData.postId}/edit`}>
+                              <a>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                  <title>Edit</title>
+                                  <path
+                                      fillRule="evenodd"
+                                      d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                                      clipRule="evenodd"
+                                  />
+                                </svg>
+                              </a>
+                            </Link>
+                          </PostOptionColumn>
                       )}
                     </>
                 )}
+                <PostOptionColumn>
+                  <label
+                      htmlFor="copy-modal"
+                      className="modal-button"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                      <title>Copy Link</title>
+                      <path fillRule="evenodd"
+                            d="M19.902 4.098a3.75 3.75 0 00-5.304 0l-4.5 4.5a3.75 3.75 0 001.035 6.037.75.75 0 01-.646 1.353 5.25 5.25 0 01-1.449-8.45l4.5-4.5a5.25 5.25 0 117.424 7.424l-1.757 1.757a.75.75 0 11-1.06-1.06l1.757-1.757a3.75 3.75 0 000-5.304zm-7.389 4.267a.75.75 0 011-.353 5.25 5.25 0 011.449 8.45l-4.5 4.5a5.25 5.25 0 11-7.424-7.424l1.757-1.757a.75.75 0 111.06 1.06l-1.757 1.757a3.75 3.75 0 105.304 5.304l4.5-4.5a3.75 3.75 0 00-1.035-6.037.75.75 0 01-.354-1z"
+                            clipRule="evenodd"/>
+                    </svg>
+                  </label>
+                </PostOptionColumn>
               </div>
             </div>
             <div className="rounded-box mt-2 flex w-full flex-wrap bg-base-200 p-4 p-4">
@@ -278,6 +308,27 @@ const Post = ({postData}: { postData: PostResponse }) => {
               )}
             </div>
             <PostCommentForm postData={postData}/>
+            <input type="checkbox" id="copy-modal" className="modal-toggle"/>
+            <label htmlFor="copy-modal" className="modal cursor-pointer">
+              <label className="modal-box relative" htmlFor="">
+                <h3 className="text-lg font-bold mb-2">Chose what to copy!</h3>
+                <div className="flex flex-col gap-2">
+                  <div>
+                    <CopyButton text={postUrl}>
+                      <button className="btn whitespace-nowrap"><a>Copy Post Link</a></button>
+                    </CopyButton>
+                  </div>
+                  {postData.type === "IMAGES" && (
+                      <div>
+                        <CopyButton
+                            text={postData.images!.slice(0, Math.min(5, postData.images!.length)).map((_, index) => `${postUrl}?image=${index}`).join("\n")}>
+                          <button className="btn whitespace-nowrap"><a>Copy Image Links</a></button>
+                        </CopyButton>
+                      </div>
+                  )}
+                </div>
+              </label>
+            </label>
           </div>
         </Layout>
       </>
@@ -312,7 +363,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const postData = await res.json();
 
   return {
-    props: {postData}
+    props: {postData, postUrl: process.env.NEXTAUTH_URL}
   };
 };
 
