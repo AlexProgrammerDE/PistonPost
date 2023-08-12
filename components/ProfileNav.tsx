60ce7d99a4cf9913860f4df8b5bf519a116bdb40
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-08-12 13:37:33 +0200

    Fixup dropdown

--- components/ProfileNav.tsx
+++ components/ProfileNav.tsx
@@ -10,7 +10,7 @@ export default function ProfileNav() {
 
   if (status === "loading" || (status === "authenticated" && !user)) {
     return (
-      <li tabIndex={0}>
+      <li>
         <a className="mx-1 flex flex-row">
           <span>Loading</span>
         </a>
@@ -20,34 +20,36 @@ export default function ProfileNav() {
 
   if (user) {
     return (
-      <li tabIndex={0}>
-        <a className="mx-1 flex flex-row">
-          <div className="avatar">
-            <Image
-              width={28}
-              height={28}
-              className="rounded-full"
-              src={user.avatar}
-              alt={`Avatar of ${user.name}`}
-            />
-          </div>
-          <span>{user.name}</span>
-        </a>
-        <ul className="left-0 top-full z-20 gap-1 bg-base-200 p-2 shadow-2xl">
-          <li>
-            <Link href="/account/settings">
-              Settings
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/posts">
-              Posts
-            </Link>
-          </li>
-          <li>
-            <a onClick={() => signOut()}>Sign out</a>
-          </li>
-        </ul>
+      <li>
+        <details>
+          <summary>
+            <div className="avatar">
+              <Image
+                width={28}
+                height={28}
+                className="rounded-full"
+                src={user.avatar}
+                alt={`Avatar of ${user.name}`}
+              />
+            </div>
+            <span>{user.name}</span>
+          </summary>
+          <ul className="bg-base-200">
+            <li>
+              <Link href="/account/settings">
+                Settings
+              </Link>
+            </li>
+            <li>
+              <Link href="/account/posts">
+                Posts
+              </Link>
+            </li>
+            <li>
+              <a onClick={() => signOut()}>Sign out</a>
+            </li>
+          </ul>
+        </details>
       </li>
     );
   } else {
